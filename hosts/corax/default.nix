432df1c11d5b2280e7914e23ee82490a93ff87cc
fabricesemti80 <fabrice@fabricesemti.com>
2024-05-18 14:38:43 +0100

    fix: ⚡️ fix deprecations

--- hosts/corax/default.nix
+++ hosts/corax/default.nix
@@ -82,8 +82,8 @@ in {
   };
 
   # Enable automatic login for the user.
-  services.xserver.displayManager.autoLogin.enable = true;
-  services.xserver.displayManager.autoLogin.user = "fs";
+  services.displayManager.autoLogin.enable = true;
+  services.displayManager.autoLogin.user = "fs";
 
   # Workaround for GNOME autologin: https://github.com/NixOS/nixpkgs/issues/103746#issuecomment-945091229
   systemd.services."getty@tty1".enable = false;
